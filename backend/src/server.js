91838f4d0a3e649ff3d3a012d607816c4fe4190c
Nikodimos Hunegn <nikodimosrodas@gmail.com>
2026-07-06 00:51:19 +0300

    Added rateLimiter and made it ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,33 @@ import notesRouter from "./routes/notesRouter.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
-// import rateLimiter from "./middleware/rateLimiter.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
-app.use(cors())
-
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors())
+}
+    
 app.use(express.json());
 
-// app.use(rateLimiter);
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
+    
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server started on port: ", PORT);
